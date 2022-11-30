1284b5df4be8cd3047f1717fa2337703d2ef9886
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-11-30 22:00:31 +0700

    version 1.2.0
    - Data

---
+++ app/src/main/java/com/example/companies/data/retrofit/factory/ApiFactory.kt
@@ -0,0 +1,17 @@
+package com.example.companies.data.retrofit.factory
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiFactory {
+    private val client = OkHttpClient.Builder().build()
+    private val BASE_URL = "https://lifehack.studio/test_task/test.php/"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+}
---
+++ app/src/main/java/com/example/companies/data/retrofit/service/CompaniesService.kt
@@ -0,0 +1,16 @@
+package com.example.companies.data.retrofit.service
+
+import com.example.companies.domain.model.Company
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CompaniesService {
+
+    @GET("{?id}")
+    fun getCompany(@Path("id") id: Int): Call<Company>
+
+    @GET()
+    fun getListCompanies(): Call<List<Company>>
+
+}
