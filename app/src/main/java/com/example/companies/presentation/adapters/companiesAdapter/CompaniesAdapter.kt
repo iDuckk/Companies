948504e7cdb6c68d15da64132b158240d26bea3a
Лачимов Егор Владимирович <55237892+iDuckk@users.noreply.github.com>
2022-12-01 20:27:59 +0700

    version 1.12.0
    - Glide в Adapter

--- app/src/main/java/com/example/companies/presentation/adapters/companiesAdapter/CompaniesAdapter.kt
+++ app/src/main/java/com/example/companies/presentation/adapters/companiesAdapter/CompaniesAdapter.kt
@@ -3,11 +3,15 @@ package com.example.companies.presentation.adapters.companiesAdapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
+import com.example.companies.R
 import com.example.companies.databinding.ItemCompanyBinding
 import com.example.companies.model.Company
+import com.example.companies.presentation.fragmentDetails.DetailsCompanyFragment
 import javax.inject.Inject
 
 var onClickListenerItem: ((item: Int) -> Unit)? = null
+const val IMAGE_URL = "https://lifehack.studio/test_task/"
 
 class CompaniesAdapter @Inject constructor() :
     ListAdapter<Company, CompaniesViewHolder>(CompaniesCallback()) {
@@ -25,6 +29,13 @@ class CompaniesAdapter @Inject constructor() :
         val item = getItem(position)
 
         holder.name.text = item.name
+
+        //Set Image
+        Glide
+            .with(holder.itemView.context)
+            .load("${IMAGE_URL}${item.img}")
+            .error(R.drawable.ic_android)
+            .into(holder.img)
 
         holder.itemView.setOnClickListener {
             onClickListenerItem?.invoke(item.id)
